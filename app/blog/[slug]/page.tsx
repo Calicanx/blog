131b6ba0d840fe438364eda9aaada926782268b1
Calicanx <rbnchris0@gmail.com>
2025-11-07 00:47:58 +0300

    fix: generateStaticParams

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,105 +5,110 @@ import { getBlogPosts } from "app/db/blog";
 import Image from "next/image";
 import formatDate from "../../utils/formatDate";
 
-export async function generateMetadata({
-	params,
-}): Promise<Metadata | undefined> {
-	let post = getBlogPosts().find(post => post.slug === params.slug);
-	if (!post) {
-		return;
-	}
+export async function generateStaticParams() {
+  const posts = getBlogPosts();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
-	let {
-		title,
-		publishedAt: publishedTime,
-		summary: description,
-	} = post.metadata;
-	// let ogImage = `https://ayushchugh.com/ogs/ogs-bg-blog-${post.slug}.png`;
-	let ogImage = post.metadata.ogImage
-		? post.metadata.ogImage
-		: `https://cdn.ayushchugh.com/open-graph/blogs.png`;
+export async function generateMetadata({ params }): Promise<Metadata | undefined> {
+  const post = getBlogPosts().find((post) => post.slug === params.slug);
+  if (!post) {
+    return;
+  }
 
-	return {
-		title,
-		description,
-		openGraph: {
-			title,
-			description,
-			type: "article",
-			publishedTime,
-			url: `https://ayushchugh.com/blog/${post.slug}`,
-			images: [
-				{
-					url: ogImage,
-				},
-			],
-		},
-		twitter: {
-			card: "summary_large_image",
-			title,
-			description,
-			images: [ogImage],
-			creator: "@aayushchugh_x",
-			creatorId: "@aayushchugh_x",
-			site: "@aayushchugh_x",
-			siteId: "@aayushchugh_x",
-		},
-	};
+  const {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+  } = post.metadata;
+
+  const ogImage = post.metadata.ogImage
+    ? post.metadata.ogImage
+    : `https://cdn.ayushchugh.com/open-graph/blogs.png`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      publishedTime,
+      url: `https://ayushchugh.com/blog/${post.slug}`,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogImage],
+      creator: "@aayushchugh_x",
+      creatorId: "@aayushchugh_x",
+      site: "@aayushchugh_x",
+      siteId: "@aayushchugh_x",
+    },
+  };
 }
 
 export default function Blog({ params }) {
-	let post = getBlogPosts().find(post => post.slug === params.slug);
+  const post = getBlogPosts().find((post) => post.slug === params.slug);
 
-	if (!post) {
-		notFound();
-	}
+  if (!post) {
+    notFound();
+  }
 
-	return (
-		<section>
-			<script
-				type="application/ld+json"
-				suppressHydrationWarning
-				dangerouslySetInnerHTML={{
-					__html: JSON.stringify({
-						"@context": "https://schema.org",
-						"@type": "BlogPosting",
-						headline: post.metadata.title,
-						datePublished: post.metadata.publishedAt,
-						dateModified: post.metadata.publishedAt,
-						description: post.metadata.summary,
-						image: post.metadata.ogImage
-							? `${post.metadata.ogImage}`
-							: `${post.metadata.coverImage}`,
-						url: `https://ayushchugh.com/blog/${post.slug}`,
-						author: {
-							"@type": "Person",
-							name: "Ayush Chugh",
-						},
-					}),
-				}}
-			/>
-			<h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
-				{post.metadata.title}
-			</h1>
-			<div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
-				<p className="text-sm text-neutral-600 dark:text-neutral-400">
-					{formatDate(post.metadata.publishedAt)}
-				</p>
-			</div>
-			{post.metadata.coverImage && (
-				<div className="my-4 rounded-lg">
-					<Image
-						src={post.metadata.coverImage}
-						alt={post.metadata.title}
-						width={1200}
-						height={630}
-						className="rounded-lg"
-					/>
-				</div>
-			)}
-			<article className="prose prose-quoteless prose-neutral dark:prose-invert">
-				<CustomMDX source={post.content} />
-			</article>
-		</section>
-	);
+  return (
+    <section>
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
+            headline: post.metadata.title,
+            datePublished: post.metadata.publishedAt,
+            dateModified: post.metadata.publishedAt,
+            description: post.metadata.summary,
+            image: post.metadata.ogImage
+              ? `${post.metadata.ogImage}`
+              : `${post.metadata.coverImage}`,
+            url: `https://ayushchugh.com/blog/${post.slug}`,
+            author: {
+              "@type": "Person",
+              name: "Ayush Chugh",
+            },
+          }),
+        }}
+      />
+      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
+        {post.metadata.title}
+      </h1>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {formatDate(post.metadata.publishedAt)}
+        </p>
+      </div>
+      {post.metadata.coverImage && (
+        <div className="my-4 rounded-lg">
+          <Image
+            src={post.metadata.coverImage}
+            alt={post.metadata.title}
+            width={1200}
+            height={630}
+            className="rounded-lg"
+          />
+        </div>
+      )}
+      <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+        <CustomMDX source={post.content} />
+      </article>
+    </section>
+  );
 }
