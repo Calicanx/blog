4a0f0e129e57f0f5b40e03db54ba73f754698e77
Calicanx <rbnchris0@gmail.com>
2025-11-07 02:18:51 +0300

    ci: fix css vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 // next.config.js
 module.exports = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/blog" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/blog/" : "",
-  // Optionally disable image optimization for static export:
   images: {
     unoptimized: true,
   },
