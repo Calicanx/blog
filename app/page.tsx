bc13a129c61ffaeda51dd0f95ecc5402e3bb2ee8
Calicanx <rbnchris0@gmail.com>
2026-02-26 12:19:33 +0300

    style: remove footer hints and social link

--- app/page.tsx
+++ app/page.tsx
@@ -168,25 +168,7 @@ export default function Page() {
 				</p>
 			</div>
 
-			<div className="prose prose-neutral dark:prose-invert">
-				<article className="text-xs sm:hidden lg:block">
-					Press ⌘+K to navigate with your keyboard.
-				</article>
-			</div>
 
-			<ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
-				<li>
-					<a
-						className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
-						rel="noopener noreferrer"
-						target="_blank"
-						href="https://x.com/chainsaww_man"
-					>
-						<ArrowIcon />
-						<p className="h-7 ml-2">Follow me</p>
-					</a>
-				</li>
-			</ul>
 
 			<div>
 				<a
